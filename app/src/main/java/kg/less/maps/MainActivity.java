f76503ba81b9cb3f1d35c19258fcc751d20493c3
zkalmamatov <jolboldu39@gmail.com>
2024-06-30 19:46:55 +0600

    добавил

--- app/src/main/java/kg/less/maps/MainActivity.java
+++ app/src/main/java/kg/less/maps/MainActivity.java
@@ -1,6 +1,9 @@
 package kg.less.maps;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Button button1 = findViewById(R.id.Button_Map);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+            }
+        });
     }
+
 }
